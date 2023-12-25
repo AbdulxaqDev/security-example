da310b5b0d0653914ecb08ceaa3242ad60d3f2cb
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-12-25 09:27:35 +0300

    authentication with google and with oauth using passport and passport-google-oauth20 to implement google one sign in.

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const path = require("path");
 const https = require("https");
 const express = require("express");
 const helmet = require("helmet");
+const passport = require("passport");
+const { Strategy } = require("passport-google-oauth20");
+
 
 require("dotenv").config();
 
@@ -13,9 +16,24 @@ const config = {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 }
 
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+} 
+
+function verifyCallback(accessToken, refreshToken, profile, done){
+  console.log("Google profile:", profile);
+  done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback);
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
+
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true // TODO
@@ -29,12 +47,25 @@ function checkLoggedIn(req, res, next) {
 
 app.get('/auth/google', (req, res) => {});
 
-app.get('/auth/google/callback', (req, res) => {});
+app.get('/auth/google/callback', 
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back!");
+  }
+);
 
 app.get('/auth/logout', (req, res) => {});
 
 app.get('/secret', checkLoggedIn, (req, res) => {
   res.send("The secret value is 42!");
+});
+
+app.get("/", (req, res) => {
+  return res.send("Failed to log in!");
 });
 
 app.get('/', (req, res) => {
