f46c0af83161564af1116c331d6015adf71db4a6
AbdulxaqDev <abdulkhak8tursunov@gmail.com>
2023-12-27 15:54:28 +0300

    set up cookie-session

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const fs = require("fs");
 const path = require("path");
 const https = require("https");
+
 const express = require("express");
 const helmet = require("helmet");
 const passport = require("passport");
 const { Strategy } = require("passport-google-oauth20");
-
+const cookieSession = require("cookie-session");
 
 require("dotenv").config();
 
+const app = express();
 const PORT = 3000;
 
 const config = {
   CLIENT_ID: process.env.CLIENT_ID,
   CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 }
 
 const AUTH_OPTIONS = {
@@ -29,9 +33,15 @@ function verifyCallback(accessToken, refreshToken, profile, done){
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
 
-const app = express();
 
 app.use(helmet());
+
+app.use(cookieSession({
+  name: "session",
+  maxAge: 24 * 60 * 60 * 1000,
+  keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
+}))
+
 app.use(passport.initialize());
 
 
@@ -48,7 +58,8 @@ function checkLoggedIn(req, res, next) {
 app.get('/auth/google', 
   passport.authenticate("google", {
     scope: ["email"]
-  }));
+  })
+);
 
 app.get('/auth/google/callback', 
   passport.authenticate("google", {
